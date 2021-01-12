3a20bdfb61d61e44e778c3714be9418d34b90c2e
Watori <ryota.hayasaka207@gmail.com>
2021-01-12 23:21:21 +0900

    Add App description componets

--- src/app.js
+++ src/app.js
@@ -1,15 +1,15 @@
 Vue.createApp({
   // アプリケーションの定義
-  // components: {
-  //   'my-title': {
-  //     data: function () {
-  //       return {
-  //         title: '',
-  //       };
-  //     },
-  //     template: '#title-template',
-  //   },
-  // },
+  components: {
+    'app-description': {
+      data: function () {
+        return {
+          title: '',
+        };
+      },
+      template: '#app-description',
+    },
+  },
   data: function () {
     return {
       todoTitle: '',
